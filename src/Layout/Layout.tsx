54c1861cdcc4dd52b39e75cbc60e1ac4ca599bd4
fferdinandpp <ferditetsuya22@gmail.com>
2025-12-04 16:34:12 +0700

    fixing layout.tsx

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import Navbar from "../components/Navbar";
+import FloatingContact from "../components/FloatingContact";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
 
-export default function Layout() {
+function Layout() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <>
-      <Navbar />
+      <Navbar onMenuChange={setSidebarOpen} />
+
       <Outlet />
       <Footer />
+
+      {/* kirim "hidden" ke floating button */}
+      <FloatingContact hidden={sidebarOpen} />
     </>
   );
 }
+
+export default Layout;
